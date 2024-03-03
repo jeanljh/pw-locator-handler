7c5e5012870a43c7c9ab0ba63b746e2547a9062f
jeanljh <jeanljh@users.noreply.github.com>
2024-03-03 09:32:38 +0000

    Deploying to gh-pages from @ jeanljh/pw-locator-handler@a1648a83ef0c9c14c5abf219cb45db14b0ed14bd 🚀

--- tests/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('addLocatorHandler', async ({ page }) => {
-  await page.goto('');
-  await page.addLocatorHandler(page.locator('#popupContainer'), async () => {
-    await page.locator('#closeButton').click();
-  })
-  await expect(page.locator('#sampleMessage')).toHaveText('This is a sample message displayed after the page has finished loading.');
-});
